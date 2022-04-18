1ed5517291aaa44d96cbd15de02ca11659d3dc81
yuan <yuanqianpeng@haohaozhu.com>
2022-04-18 09:43:15 +0800

    配置项结构体调整

--- config/config.go
+++ config/config.go
@@ -1,7 +1,12 @@
 package config
 
 type Config struct {
-	Ip       string
-	GrpcPort string
-	HttpPort string
+	Ip            string // 服务器IP
+	GrpcPort      string // gRPC服务端口号
+	PortReuse     bool   // http服务与gRPC服务是否端口复用，仅在开启RunHTTP为true时候有效
+	RunHTTP       bool   // 是否开启http服务
+	HttpPort      string // http服务所在端口号，仅在开启RunHTTP为true时且PortReuse为false时有效
+	TLSKey        string // 证书
+	TLSPerm       string // 证书
+	RunReflection bool   // 是否开启gRPC服务反射
 }
